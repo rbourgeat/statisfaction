9fd95cd90bf69525b01f361f1e0889f28579ec3e
krolhm <raphaelb1@icloud.com>
2024-11-21 03:18:14 +0100

    Refacto frontend

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,6 +3,19 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import {
+  FaRegCircleCheck,
+  FaFaceFrown,
+  FaFaceLaughBeam,
+  FaFaceDizzy,
+  FaFaceSadCry,
+  FaHourglassHalf,
+  FaCircleUp,
+  FaCircleDown,
+  FaRegCircleXmark,
+  FaRegCircleQuestion,
+  FaRegCalendarXmark
+} from "react-icons/fa6";
 
 export default function Home() {
   const [statuses, setStatuses] = useState([]);
@@ -35,150 +48,175 @@ export default function Home() {
       fetchStatusData();
       fetchIncidents();
     }, 2000);
-  
+
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="relative min-h-screen overflow-hidden">
-      <div className="relative z-10 p-8">
-        <h1 className="text-4xl font-extrabold text-center text-transparent bg-clip-text bg-white mb-8">
-          {title}
-        </h1>
-        <div className="space-y-8">
-          {statuses.map((status, index) => {
-            const last90Days = status.dailyHistory || [];
-            const today = new Date();
+    <div className="min-h-screen bg-[#0b0c14] text-white flex flex-col items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold mb-4 mt-6">{title}</h1>
+          <p className="text-gray-400 mb-8">Check the status of our services below.</p>
+        </div>
+        <div className="max-w-4xl p-4 mb-2 p-2 rounded-xl backdrop-blur-2xl border border-[#727DA1]/20 bg-[#171824]/80">
+          <div className="grid grid-cols-1 gap-2">
+            {statuses.map((status, index) => {
+              const last90Days = status.dailyHistory || [];
+              const today = new Date();
 
-            const daysToDisplay = Array.from({ length: 90 }, (_, i) => {
-              const date = new Date(today);
-              date.setDate(today.getDate() - (89 - i));
-              const formattedDate = date.toISOString().split("T")[0];
+              const daysToDisplay = Array.from({ length: 90 }, (_, i) => {
+                const date = new Date(today);
+                date.setDate(today.getDate() - (89 - i));
+                const formattedDate = date.toISOString().split("T")[0];
 
-              const dayData = last90Days.find((day) => day.date === formattedDate);
+                const dayData = last90Days.find((day) => day.date === formattedDate);
+                return (
+                  dayData || {
+                    date: formattedDate,
+                    uptime: null,
+                    downtimeHours: null,
+                  }
+                );
+              });
+
+              const averageUptime = (
+                daysToDisplay.reduce(
+                  (sum, day) => sum + (day.uptime !== null ? day.uptime : 100),
+                  0
+                ) / 90
+              ).toFixed(2);
+
               return (
-                dayData || {
-                  date: formattedDate,
-                  uptime: null,
-                  downtimeHours: null,
-                }
-              );
-            });
-
-            const averageUptime = (
-              daysToDisplay.reduce(
-                (sum, day) => sum + (day.uptime !== null ? day.uptime : 100),
-                0
-              ) / 90
-            ).toFixed(2);
-
-            return (
-              <div key={index} className="relative p-6">
-                <div
-                  className={`absolute inset-0 rounded-lg ${
-                    status.lastStatus === null
-                      ? "bg-gray-500"
-                      : status.lastStatus
-                      ? "bg-green-500"
-                      : "bg-red-500"
-                  } z-0 scale-x-90 scale-y-50 origin-right -translate-x-10`}
-                ></div>
-
-                <div className="relative p-6 bg-white/10 backdrop-blur-xl rounded-lg shadow-xl z-10">
-                  <div className={`absolute font-semibold cursor-pointer top-0 right-0 m-2 text-xs py-1 px-2 leading-none bg-gray-800 rounded-md
-                    ${status.lastStatus === null
-                      ? "text-gray-500"
-                      : status.lastStatus
-                      ? "text-green-500"
-                      : "text-red-500"
-                    }
-                  `}>
-                    <span className="relative inline-flex items-center group">
-                    {averageUptime}% uptime
-                      <div className="absolute left-1/2 bottom-full mb-2 hidden max-w-max w-auto whitespace-nowrap px-4 py-2 text-xs font-medium text-white bg-black rounded-md shadow-md -translate-x-1/2 group-hover:block backdrop-blur-lg bg-opacity-70">
-                        Ping every {status.pingInterval / 1000}s
-                      </div>
-                    </span>
-                  </div>
-                  <h2 className="text-2xl font-semibold text-white flex items-center">
-                    {status.name}
-                    {status.showIp && <a href={status.address}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className={`cursor-pointer m-2 text-xs py-1 px-2 leading-none bg-gray-800 rounded-md
-                      ${status.lastStatus === null
-                        ? "text-gray-500"
-                        : status.lastStatus
-                        ? "text-green-500"
-                        : "text-red-500"
-                      }
-                    `}>
-                      {status.address}
-                    </a>}
-                  </h2>
-                  <div className="mt-4 flex space-x-1">
-                    {daysToDisplay.map((day, idx) => (
-                      <div
-                        key={idx}
-                        className="group relative w-1 h-8"
-                      >
-                        <div className="absolute left-1/2 bottom-full mb-2 hidden max-w-max w-auto whitespace-nowrap px-4 py-2 text-xs font-medium text-white bg-black rounded-md shadow-md -translate-x-1/2 group-hover:block backdrop-blur-lg bg-opacity-70">
-                          <p>{day.date}</p>
-                          {day.uptime !== null ? (
-                            <>
-                              <p>Uptime: {day.uptime.toFixed(2)}%</p>
-                              <p>Downtime: {day.downtimeHours?.toFixed(1) || 0} hrs</p>
-                            </>
+                <div key={index} className="relative">
+                  <div className="p-6">
+                    <div className="flex justify-between items-center">
+                      <h2 className="font-semibold text-white flex items-center">
+                        <div className={`text-md mr-2 bg-[#171824]/80`}>
+                          {status.lastStatus === null ? (
+                            <FaRegCircleQuestion className="text-gray-500" />
+                          ) : status.lastStatus ? (
+                            <FaRegCircleCheck className="text-green-500" />
                           ) : (
-                            <p>No data available</p>
+                            <FaRegCircleXmark className="text-red-500" />
                           )}
                         </div>
+                        {status.name}
+                      </h2>
 
-                        <div
-                          className={`w-full h-full cursor-pointer rounded-md shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-150 ${
-                            day.uptime === null
-                              ? "bg-gray-400 bg-opacity-50"
-                              : day.uptime >= 75
-                              ? "bg-green-400"
-                              : day.uptime >= 50
-                              ? "bg-yellow-400"
-                              : day.uptime >= 25
-                              ? "bg-orange-400"
-                              : "bg-red-400"
+                      <div
+                        className={`font-semibold cursor-pointer text-xs
+                          ${averageUptime === null
+                            ? "text-gray-400"
+                            : averageUptime >= 75
+                            ? "text-green-400"
+                            : averageUptime >= 50
+                            ? "text-yellow-400"
+                            : averageUptime >= 25
+                            ? "text-orange-400"
+                            : "text-red-400"
                           }`}
-                        />
+                      >
+                        <span className="relative items-center group flex justify-center">
+                          <h2 className="text-center">
+                            {averageUptime}% uptime
+                          </h2>
+                          <div className="absolute left-1/2 mb-2 hidden max-w-max w-auto whitespace-nowrap px-4 py-2 text-xs font-medium text-white translate-x-1/2 group-hover:block rounded-xl backdrop-blur-2xl border border-[#727DA1]/20 bg-[#171824]/80">
+                            <p className="flex items-center space-x-2 text-gray-400">
+                              <FaHourglassHalf />
+                              <span>Ping every {status.pingInterval / 1000}s</span>
+                            </p>
+                          </div>
+                        </span>
                       </div>
-                    ))}
+                    </div>
+                    <div className="mt-4 flex space-x-1">
+                      {daysToDisplay.map((day, idx) => (
+                        <div
+                          key={idx}
+                          className="group relative w-1 h-8"
+                        >
+                          <div className="absolute left-1/2 bottom-full mb-4 hidden max-w-max w-auto whitespace-nowrap px-4 py-2 text-xs font-medium text-white -translate-x-1/2 group-hover:block rounded-xl backdrop-blur-2xl border border-[#727DA1]/20 bg-[#171824]/80">
+                            {day.uptime !== null ? (
+                              <div>
+                                <div className="border-b border-gray-700 pb-2 mb-2 text-center font-bold">
+                                  {day.uptime >= 75 ? (
+                                    <p className="flex items-center space-x-2 text-green-400">
+                                      <FaFaceLaughBeam className="text-lg" />
+                                      <span>Operational</span>
+                                    </p>
+                                  ) : day.uptime >= 50 ? (
+                                    <p className="flex items-center space-x-2 text-yellow-400">
+                                      <FaFaceFrown className="text-lg" />
+                                      <span>Degraded</span>
+                                    </p>
+                                  ) : day.uptime >= 25 ? (
+                                    <p className="flex items-center space-x-2 text-orange-400">
+                                      <FaFaceSadCry className="text-lg" />
+                                      <span>Partial Outage</span>
+                                    </p>
+                                  ) : (
+                                    <p className="flex items-center space-x-2 text-red-400">
+                                      <FaFaceDizzy className="text-lg" />
+                                      <span>Major Outage</span>
+                                    </p>
+                                  )}
+                                </div>
+                                <p className="flex items-center space-x-2">
+                                  <FaCircleUp />
+                                  <span>Uptime: {day.uptime.toFixed(2)}%</span>
+                                </p>
+                                <p className="flex items-center space-x-2">
+                                  <FaCircleDown />
+                                  <span>Downtime: {day.downtimeHours?.toFixed(1) || 0} h</span>
+                                </p>
+                              </div>
+                            ) : (
+                              <p>No data available</p>
+                            )}
+                            <p className="border-t border-gray-700 pt-2 mt-2 text-center text-gray-400">
+                              {new Date(day.date).getDate()} {new Date(day.date).toLocaleString('default', { month: 'short' })} {new Date(day.date).getFullYear()}
+                            </p>
+                          </div>
+
+                          <div
+                            className={`w-full h-full cursor-pointer rounded-md shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-150 ${
+                              day.uptime === null
+                                ? "bg-gray-400 bg-opacity-50"
+                                : day.uptime >= 75
+                                ? "bg-green-400"
+                                : day.uptime >= 50
+                                ? "bg-yellow-400"
+                                : day.uptime >= 25
+                                ? "bg-orange-400"
+                                : "bg-red-400"
+                            }`}
+                          />
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
 
-        <div className="mt-16">
-          <h2 className="text-2xl font-semibold text-white mb-4">Past Incidents</h2>
+        <h2 className="m-16 text-2xl font-semibold text-white mb-4">Past Incidents</h2>
+        <div className="m-4 p-4 w-[60%] rounded-xl backdrop-blur-2xl border border-[#727DA1]/20 bg-[#171824]/80">
           {incidents.length > 0 ? (
             incidents.map((incident, index) => (
-              <div key={index} className="mb-8">
+              <div key={index} className="m-4 pl-8">
                 <a
                   href={incident.issueUrl}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-lg font-bold hover:no-underline text-red-400 hover:underline"
+                  className="text-lg font-bold hover:no-underline text-red-400 hover:underline flex items-center space-x-2"
                 >
-                  {incident.title}
+                  <FaRegCalendarXmark />
+                  <span>{incident.title}</span>
                 </a>
                 <p className="text-sm text-gray-400">
-                {
-                  new Date(incident.createdAt).toLocaleString("en-GB", {
-                    day: "2-digit",
-                    month: "short",
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    hour12: false,
-                  }).replace(",", "").replace(":", "h")
-                }
+                {new Date(incident.createdAt).getDate()} {new Date(incident.createdAt).toLocaleString('default', { month: 'short' })} {new Date(incident.createdAt).getFullYear()} {new Date(incident.createdAt).toLocaleString("en-GB", {hour: "2-digit", minute: "2-digit", hour12: false,}).replace(",", "").replace(":", "h")}
                 </p>
               </div>
             ))
@@ -186,8 +224,6 @@ export default function Home() {
             <p className="text-gray-500">No incidents reported this month.</p>
           )}
         </div>
-
-      </div>
     </div>
   );
 }
